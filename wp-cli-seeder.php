d6ceb818ccbb7426f77cd1c50e05d82276d04841
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2024-02-25 20:03:32 +0100

    Added entry file

---
+++ wp-cli-seeder.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: WP CLI Seeder
+ * Plugin URI: https://alessandrotesoro.me
+ * Description: A WP CLI command to seed your WordPress database with dummy data.
+ * Version: 0.1.0
+ * Author Name: Alessandro Tesoro
+ * Author URI: https://alessandrotesoro.me
+ */
+
+defined('ABSPATH') || exit;
+
+require dirname(__FILE__) . '/vendor/autoload.php';
+
+add_action( 'plugins_loaded', function() {
+
+} );
