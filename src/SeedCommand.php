edace32efe901de22e95a6ddc02067521c0ff499
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2024-02-27 22:46:00 +0100

    Delete existing terms from "brand" and "type" taxonomies

--- src/SeedCommand.php
+++ src/SeedCommand.php
@@ -295,6 +295,22 @@ class SeedCommand {
 	 * @return void
 	 */
 	private function process_attributes( $products ) {
+		// First delete all existing terms from the "brand" and "type" taxonomies.
+		$this->delete_terms(
+			null,
+			[
+				'taxonomy' => 'pa_brand',
+				'force'    => true,
+			]
+		);
+		$this->delete_terms(
+			null,
+			[
+				'taxonomy' => 'pa_product_type',
+				'force'    => true,
+			]
+		);
+
 		// Collect the "brand" from the products array.
 		$brands = array_column( $products, 'brand' );
 
@@ -413,5 +429,58 @@ class SeedCommand {
 		$progress->finish();
 
 		WP_CLI::success( "All {$post_type} have been deleted." );
+	}
+
+	/**
+	 * Delete all terms of a given taxonomy from the database.
+	 *
+	 * ## OPTIONS
+	 *
+	 * [--taxonomy=<string>]
+	 * : The taxonomy to delete terms from.
+	 * ---
+	 * default: category
+	 * ---
+	 *
+	 * [--force]
+	 * : Force the deletion without confirmation.
+	 * ---
+	 * default: false
+	 * ---
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     wp seed delete_terms --taxonomy=category
+	 *
+	 * @when after_wp_load
+	 */
+	public function delete_terms( $args, $assoc_args ) {
+		$taxonomy = $assoc_args['taxonomy'];
+
+		// Confirm the deletion.
+		if ( ! \WP_CLI\Utils\get_flag_value( $assoc_args, 'force' ) ) {
+			WP_CLI::confirm( "Are you sure you want to delete all terms from {$taxonomy}?" );
+		}
+
+		// Query all terms of the given taxonomy and delete them.
+		$terms = get_terms(
+			[
+				'taxonomy'   => $taxonomy,
+				'hide_empty' => false,
+				'fields'     => 'ids',
+			]
+		);
+
+		$progress = \WP_CLI\Utils\make_progress_bar( "Deleting all terms from {$taxonomy}", count( $terms ) );
+
+		foreach ( $terms as $term ) {
+			wp_delete_term( $term, $taxonomy );
+
+			$progress->tick();
+		}
+
+		$progress->finish();
+
+		WP_CLI::success( "All terms from {$taxonomy} have been deleted." );
 	}
 }
