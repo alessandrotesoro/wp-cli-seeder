af52b17fdea1c7a1445bb58dc9ead1780791d1b7
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2024-04-01 21:06:59 +0200

    Add fakerphp/faker dependency and seed users command

--- src/SeedUsersCommand.php
+++ src/SeedUsersCommand.php
@@ -10,6 +10,7 @@
 
 namespace Sematico\Seeder;
 
+use Faker\Factory;
 use WP_CLI;
 
 class SeedUsersCommand {
@@ -52,4 +53,58 @@ class SeedUsersCommand {
 		WP_CLI::success( 'All users have been deleted.' );
 	}
 
+	/**
+	 * Seed the database with dummy users.
+	 *
+	 * ## OPTIONS
+	 *
+	 * [--number=<number>]
+	 * : How many users to generate.
+	 * ---
+	 * default: 10
+	 * ---
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     wp seed users generate --number=10
+	 *
+	 * @when after_wp_load
+	 *
+	 * @param array $args       Command arguments.
+	 * @param array $assoc_args Command associative arguments.
+	 */
+	public function generate( $args, $assoc_args ) {
+		$number = isset( $assoc_args['number'] ) ? absint( $assoc_args['number'] ) : 10;
+
+		if ( $number < 1 ) {
+			WP_CLI::error( 'The number of users to generate must be greater than 0.' );
+		}
+
+		$faker = Factory::create();
+
+		$progress = \WP_CLI\Utils\make_progress_bar( 'Generating users', $number );
+
+		for ( $i = 0; $i < $number; $i++ ) {
+			$user_id = wp_insert_user(
+				[
+					'user_login' => $faker->userName,
+					'user_pass'  => wp_generate_password(),
+					'user_email' => $faker->email,
+					'first_name' => $faker->firstName,
+					'last_name'  => $faker->lastName,
+					'role'       => 'subscriber',
+				]
+			);
+
+			if ( is_wp_error( $user_id ) ) {
+				WP_CLI::error( $user_id->get_error_message() );
+			}
+
+			$progress->tick();
+		}
+
+		$progress->finish();
+
+		WP_CLI::success( sprintf( 'Successfully generated %d users.', $number ) );
+	}
 }
